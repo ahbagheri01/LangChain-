2e3f15552dd9b17bff2538b20dde7ceadd3c5f39
Amirhosssein <ahbagheri01@gmail.com>
2025-07-22 10:18:53 +0330

    note that triming is possible

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from llm_api import get_model
 from prompt_api import get_sample
-from work_flow_api import get_sample_w
+from work_flow_api import *
 load_dotenv()
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate
@@ -24,12 +24,32 @@ output["messages"][-1].pretty_print()
 
 print("DONE")
 
-prompt_template = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a helpful assistant. Answer all questions to the best of your ability in {language}.",
-        ),
-        MessagesPlaceholder(variable_name="messages"),
-    ]
-)
+app2 = get_sample_w2()
+config = {"configurable": {"thread_id": "abc3443"}}
+query = "Hi! I'm Jim."
+
+input_messages = [HumanMessage(query)]
+output = app2.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()
+
+query = "What is my name?"
+
+input_messages = [HumanMessage(query)]
+output = app2.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()
+
+
+
+app3 = get_sample_w3()
+config = {"configurable": {"thread_id": "abc3443"}}
+query = "Hi! I'm Jim."
+language = "Persia"
+input_messages = [HumanMessage(query)]
+output = app3.invoke({"messages": input_messages, "language": language, "name":"AMIR"}, config)
+output["messages"][-1].pretty_print()
+
+query = "What is your name?"
+
+input_messages = [HumanMessage(query)]
+output = app3.invoke({"messages": input_messages, "language": language, "name":"AMIR"}, config)
+output["messages"][-1].pretty_print()
--- work_flow_api.py
+++ work_flow_api.py
@@ -3,7 +3,7 @@ from langgraph.graph import START, MessagesState, StateGraph
 from llm_api import get_model
 # Define a new graph
 
-
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 
 def get_sample_w():
@@ -24,3 +24,70 @@ def get_sample_w():
     memory = MemorySaver()
     app = workflow.compile(checkpointer=memory)
     return app
+
+
+def get_sample_w2():
+
+
+    prompt_template = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You talk like a pirate. Answer all questions to the best of your ability.",
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+        ]
+    )
+    workflow = StateGraph(state_schema=MessagesState)
+    model = get_model()
+
+    def call_model(state: MessagesState):
+        prompt = prompt_template.invoke(state)
+        response = model.invoke(prompt)
+        return {"messages": response}
+
+
+
+# Define the (single) node in the graph
+    workflow.add_edge(START, "model")
+    workflow.add_node("model", call_model)
+
+# Add memory
+    memory = MemorySaver()
+    app = workflow.compile(checkpointer=memory)
+    return app
+from typing import Sequence
+from typing_extensions import Annotated, TypedDict
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+class State(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    language: str
+    name: str
+def get_sample_w3():
+    prompt_template = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "Your name is {name} talk like a person from {language} Speaking in {language}. Answer all questions to the best of your ability.",
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+        ]
+    )
+    workflow = StateGraph(state_schema=State)
+    model = get_model()
+
+    def call_model(state: State):
+        prompt = prompt_template.invoke(state)
+        response = model.invoke(prompt)
+        return {"messages": response}
+
+    # Define the (single) node in the graph
+    workflow.add_edge(START, "model")
+    workflow.add_node("model", call_model)
+
+    # Add memory
+    memory = MemorySaver()
+    app = workflow.compile(checkpointer=memory)
+    return app
+
